8cc22663b258a6a208b3d9f384b970424f6ec909
zhaodahan <1791154630@qq.com>
2021-06-03 23:06:20 +0800

    将payment 和order 配置为  Eureka clinet 端

--- cloud-provider-payment8001/src/main/java/com/atiguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atiguigu/springcloud/PaymentMain8001.java
@@ -2,9 +2,11 @@ package com.atiguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //这里是微服务的提供者
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
