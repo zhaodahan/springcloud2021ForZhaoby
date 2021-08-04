c248159a36ccd774e6b01934fd54b31bdfa2dd34
zhaodahan <1791154630@qq.com>
2021-08-04 22:52:14 +0800

    调试自己尝试使用nacos来作为服务注册是成功地

--- cloudalibaba-consumer-order801/src/main/java/com/atguigu/springcloud/OrderAlibabaMain81.java
+++ cloudalibaba-consumer-order801/src/main/java/com/atguigu/springcloud/OrderAlibabaMain81.java
@@ -3,9 +3,11 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class OrderAlibabaMain81{
     public static void main(String[] args) {
         SpringApplication.run(OrderAlibabaMain81.class,args);
