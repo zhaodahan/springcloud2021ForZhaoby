410761a50217534e7b49363422eac78dbfcacc35
zhaodahan <1791154630@qq.com>
2021-08-15 21:48:36 +0800

    sentinel 流控 @SentinelResource 注解

--- cloudalibaba-provider-payment9001/src/main/java/com/atiguigu/springCloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9001/src/main/java/com/atiguigu/springCloud/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.atiguigu.springCloud.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,17 @@ public class PaymentController {
     private String testFileContent;
 
     @GetMapping(value = "/payment/nacos/{id}")
-    @SentinelResource("/payment")
+    @SentinelResource(value = "payment",blockHandler  = "handleException")
     public String getPayment(@PathVariable("id") Integer id)
     {
         return "nacos registry, serverPort: "+ serverPort+"\t id"+id+"\t yml file content"+testFileContent;
+    }
+
+
+    public String handleException(@PathVariable("id") Integer id,BlockException exception)
+    {
+        System.out.println("进入了自定义的限流异常方法");
+        return "自定义限流handleException";
     }
 
 }
